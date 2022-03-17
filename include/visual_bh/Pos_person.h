1b320b1f6b7b6f478eafe323eb7803fb061e5b87
larapoves <poveslara@gmail.com>
2022-03-17 13:44:59 +0100

    constructor arreglado
    Signed-off-by: larapoves <poveslara@gmail.com>

--- include/visual_bh/Pos_person.h
+++ include/visual_bh/Pos_person.h
@@ -1,5 +1,5 @@
-#ifndef VISUAL_BH_TFPERSON_H
-#define VISUAL_BH_TFPERSON_H
+#ifndef VISUAL_BH_POS_PERSON_H
+#define VISUAL_BH_POS_PERSON_H
 
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
@@ -10,33 +10,32 @@
 #include <sensor_msgs/Image.h>
 #include <darknet_ros_msgs/BoundingBoxes.h>
 
-#include "tf2/transform_datatypes.h"
-#include "tf2/LinearMath/Transform.h"
-#include "geometry_msgs/TransformStamped.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
-#include "tf2/convert.h"
-#include "tf2_ros/transform_broadcaster.h"
-#include "tf2/LinearMath/Quaternion.h"
-
 #include "ros/ros.h"
 
 namespace visual_bh
 {
 
-class Tfperson
+class Pos_person
 {
   public:
-    Tfperson(bool update);
+    Pos_person();
 
-    bool get_update() {return update_;}
-    geometry_msgs::TransformStamped generate_tf();
+    double x() {return x_;}
+    double y() {return y_;}
     void callback_bbx(const sensor_msgs::ImageConstPtr& image, const darknet_ros_msgs::BoundingBoxesConstPtr& boxes);
 
   private:
     ros::NodeHandle nh;
-    float x_;
-    float y_;
-    bool update_;
+
+    double x_;
+    double y_;
+
+    message_filters::Subscriber<sensor_msgs::Image> image_depth_sub;
+    message_filters::Subscriber<darknet_ros_msgs::BoundingBoxes> bbx_sub;
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, darknet_ros_msgs::BoundingBoxes> MySyncPolicy_bbx;
+    message_filters::Synchronizer<MySyncPolicy_bbx> sync_bbx;
 };
 
-}
+}  // namespace visual_bh
+
+#endif  // VISUAL_BH_POS_PERSON_H
--- src/visual_bh/Pos_person.cpp
+++ src/visual_bh/Pos_person.cpp
@@ -1,4 +1,4 @@
-#include "visual_bh/Tfperson.h"
+#include "visual_bh/Pos_person.h"
 
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
@@ -9,55 +9,20 @@
 #include <sensor_msgs/Image.h>
 #include <darknet_ros_msgs/BoundingBoxes.h>
 
-#include "tf2/transform_datatypes.h"
-#include "tf2/LinearMath/Transform.h"
-#include "geometry_msgs/TransformStamped.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
-#include "tf2/convert.h"
-#include "tf2_ros/transform_broadcaster.h"
-#include "tf2/LinearMath/Quaternion.h"
-
 #include "ros/ros.h"
 
 namespace visual_bh
 {
 
-Tfperson::Tfperson(bool update)
+Pos_person::Pos_person()
+: image_depth_sub(nh, "/camera/depth/image_raw", 1), bbx_sub(nh, "/darknet_ros/bounding_boxes", 1), 
+sync_bbx(MySyncPolicy_bbx(10), image_depth_sub, bbx_sub)
 {
-    update_ = update;
-    message_filters::Subscriber<sensor_msgs::Image> image_depth_sub(nh, "/camera/depth/image_raw", 1);
-    message_filters::Subscriber<darknet_ros_msgs::BoundingBoxes> bbx_sub(nh, "/darknet_ros/bounding_boxes", 1);
-
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, darknet_ros_msgs::BoundingBoxes> MySyncPolicy_bbx;
-    message_filters::Synchronizer<MySyncPolicy_bbx> sync_bbx(MySyncPolicy_bbx(10), image_depth_sub, bbx_sub);
-    sync_bbx.registerCallback(boost::bind(&Tfperson::callback_bbx, this, _1, _2));
-}
-
-geometry_msgs::TransformStamped 
-Tfperson::generate_tf()
-{
-    tf2::Stamped<tf2::Transform> object;
-    object.frame_id_ = "base_footprint";
-    object.stamp_ = ros::Time::now();
-
-    std::cerr << "x(dits) " << x_ << " y(x) " << y_;
-
-    object.setOrigin(tf2::Vector3(x_, y_, 0.0));
-
-    tf2::Quaternion q;
-    q.setRPY(0.0, 0.0, 0.0);
-    object.setRotation(q);
-
-    geometry_msgs::TransformStamped object_msg = tf2::toMsg(object);
-    object_msg.child_frame_id = "bf2person";
-
-    update_ = false;
-
-    return object_msg;
+    sync_bbx.registerCallback(boost::bind(&Pos_person::callback_bbx, this, _1, _2));
 }
 
 void 
-Tfperson::callback_bbx(const sensor_msgs::ImageConstPtr& image, const darknet_ros_msgs::BoundingBoxesConstPtr& boxes)
+Pos_person::callback_bbx(const sensor_msgs::ImageConstPtr& image, const darknet_ros_msgs::BoundingBoxesConstPtr& boxes)
 {
     cv_bridge::CvImagePtr img_ptr_depth;
     ROS_INFO("estoy en el callback");
@@ -76,7 +41,6 @@ Tfperson::callback_bbx(const sensor_msgs::ImageConstPtr& image, const darknet_ro
 
         x_ = img_ptr_depth->image.at<float> (cv::Point(px, py));
         y_ = px;
-        update_ = true;
     }
 }
 
