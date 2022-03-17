c21aef3c0b11c68b63443d8e14f90ff3f780201c
larapoves <poveslara@gmail.com>
2022-03-17 13:16:10 +0100

    nuevos nombres
    Signed-off-by: larapoves <poveslara@gmail.com>

---
+++ include/visual_bh/Pos_person.h
@@ -0,0 +1,42 @@
+#ifndef VISUAL_BH_TFPERSON_H
+#define VISUAL_BH_TFPERSON_H
+
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+#include <cv_bridge/cv_bridge.h>
+
+#include <sensor_msgs/Image.h>
+#include <darknet_ros_msgs/BoundingBoxes.h>
+
+#include "tf2/transform_datatypes.h"
+#include "tf2/LinearMath/Transform.h"
+#include "geometry_msgs/TransformStamped.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2/convert.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2/LinearMath/Quaternion.h"
+
+#include "ros/ros.h"
+
+namespace visual_bh
+{
+
+class Tfperson
+{
+  public:
+    Tfperson(bool update);
+
+    bool get_update() {return update_;}
+    geometry_msgs::TransformStamped generate_tf();
+    void callback_bbx(const sensor_msgs::ImageConstPtr& image, const darknet_ros_msgs::BoundingBoxesConstPtr& boxes);
+
+  private:
+    ros::NodeHandle nh;
+    float x_;
+    float y_;
+    bool update_;
+};
+
+}
--- src/node_follow_person.cpp
+++ src/node_follow_person.cpp
@@ -1,3 +1,5 @@
+#include "visual_bh/Tfperson.h"
+
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
@@ -7,61 +9,49 @@
 #include <sensor_msgs/Image.h>
 #include <darknet_ros_msgs/BoundingBoxes.h>
 
+#include "tf2/transform_datatypes.h"
+#include "tf2/LinearMath/Transform.h"
+#include "geometry_msgs/TransformStamped.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2/convert.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2/LinearMath/Quaternion.h"
+
+#include "ros/ros.h"
+
 #include "geometry_msgs/Twist.h"
 
-void callback_bbx(const sensor_msgs::ImageConstPtr& image, const darknet_ros_msgs::BoundingBoxesConstPtr& boxes)
-{
-  ros::NodeHandle n;
-  ros::Publisher pub_vel = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
-
-  geometry_msgs::Twist cmd;
-
-  cmd.linear.y = 0.0;
-  cmd.linear.z = 0.0;
-  cmd.angular.x = 0.0;
-  cmd.angular.y = 0.0;
-
-  cv_bridge::CvImagePtr img_ptr_depth;
-
-  try{
-    img_ptr_depth = cv_bridge::toCvCopy(*image, sensor_msgs::image_encodings::TYPE_32FC1);
-  }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("cv_bridge exception:  %s", e.what());
-    return;
-  }
-  
-  for (const auto & box : boxes->bounding_boxes) {
-    int px = (box.xmax + box.xmin) / 2;
-    int py = (box.ymax + box.ymin) / 2;
-
-    float dist = img_ptr_depth->image.at<float>(cv::Point(px, py)) * 0.001f;
-
-    std::cerr << box.Class << " at dist: " << dist << " px (py): " << px << std::endl;
-
-    cmd.linear.x = dist - 1;
-    //angulo
-
-    pub_vel.publish(cmd);
-  }
-}
+#define OBJ_PUB_RATE  10.0
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "vision_node");
+  ros::init(argc, argv, "pub_tf_person");
 
-  ros::NodeHandle nh;
-
-  message_filters::Subscriber<sensor_msgs::Image> image_depth_sub(nh, "/camera/depth/image_raw", 1);
-  message_filters::Subscriber<darknet_ros_msgs::BoundingBoxes> bbx_sub(nh, "/darknet_ros/bounding_boxes", 1);
-
-  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, darknet_ros_msgs::BoundingBoxes> MySyncPolicy_bbx;
-  message_filters::Synchronizer<MySyncPolicy_bbx> sync_bbx(MySyncPolicy_bbx(10), image_depth_sub, bbx_sub);
-
-  sync_bbx.registerCallback(boost::bind(&callback_bbx, _1, _2));
-
-  ros::spin();
+  visual_bh::Tfperson tf_person(false);
+  tf2_ros::TransformBroadcaster br;
+  geometry_msgs::TransformStamped bf2person;
+  ros::Rate loop_rate(10);
+  
+  while(ros::ok)
+  {
+    ROS_INFO("holis");
+    if (tf_person.get_update())
+    {
+      try
+      {
+        bf2person = tf_person.generate_tf();
+        bf2person.header.stamp = ros::Time::now();
+        br.sendTransform(bf2person);
+      }
+      catch(tf2::TransformException &exception)
+      {
+        ROS_ERROR("%s", exception.what());
+      }
+    }
+    ROS_INFO("holis");
+    ros::spin();
+    loop_rate.sleep();
+  }
 
   return 0;
 }
